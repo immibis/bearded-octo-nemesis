928ef5c82d5f03bc1d29b62421072571d386ddaf
Lymia Aluysia <lymiahugs@gmail.com>
2013-05-12 07:01:20 -0500

    Include CLI in build.

--- build.bat
+++ build.bat
@@ -6,6 +6,7 @@ mkdir bin
 
 echo *** Compiling BON class files...
 javac -d bin -sourcepath . immibis\bon\gui\GuiMain.java
+javac -d bin -sourcepath . immibis\bon\cui\MCPRemap.java
 
 echo *** Packing bon.jar...
 cd bin
--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ mkdir bin
 
 echo "*** Compiling BON class files..."
 javac -d bin -sourcepath . immibis/bon/gui/GuiMain.java
+javac -d bin -sourcepath . immibis/bon/cui/MCPRemap.java
+
 
 echo "*** Packing bon.jar..."
 cd bin
