8ca49c36fca43f6dc0a7d82f270dcc9e04201d3e
Taylor Shuler <taylorshuler@aol.com>
2013-08-10 16:00:26 -0400

    Revert "Cleaned up build.sh"
    This reverts commit 2a7c5bd4eb8873d816ada7eb88753239e691a6dc.

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+
 echo "*** Cleaning bin folder..."
-if [-e bin] then rm -rf bin fi mkdir bin
+if [ -e bin ]
+then
+  rm -rf bin
+fi
+mkdir bin
 
 echo "*** Compiling BON class files..."
 javac -d bin -sourcepath . immibis/bon/gui/GuiMain.java
 javac -d bin -sourcepath . immibis/bon/cui/MCPRemap.java
+
 
 echo "*** Packing bon.jar..."
 cd bin
